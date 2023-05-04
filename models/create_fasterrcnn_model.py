6a8f66be4c8d7c3ad6269d0c4eaf38189969f3d4
GILLYBOEUF <marie-alix.gillyboeeuf@epfl.ch>
2023-05-04 16:44:57 +0200

    models updated

--- models/create_fasterrcnn_model.py
+++ models/create_fasterrcnn_model.py
@@ -206,6 +206,7 @@ def return_keypointrcnn_resnet50_fpn(
     model = keypointrcnn_resnet50_fpn.create_model(
         num_classes, pretrained, coco_model=coco_model
     )
+    return model
 
 create_model = {
     'fasterrcnn_resnet50_fpn': return_fasterrcnn_resnet50_fpn,
