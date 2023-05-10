cc20c5f6401642c46d2c8d3d182c34c5ddebfe56
GILLYBOEUF <marie-alix.gillyboeeuf@epfl.ch>
2023-05-10 12:45:14 +0200

    correct format of json annotations

--- data_kpts/check_bbox_size.py
+++ data_kpts/check_bbox_size.py
@@ -6,17 +6,31 @@ import os
 def ckeck_bbox_size(path_to_image, path_to_annotations):
     img = cv2.imread(path_to_image)
     height, width, _ = img.shape
-
+    new_annotations = {}
     with open(path_to_annotations, 'r') as f:
         annotations = json.load(f)
-        bboxes = annotations['bboxes'][0]
+        if (len(annotations['bboxes'])) == 4:
+            bboxes = annotations['bboxes']
+        else :
+            bboxes = annotations['bboxes'][0]
         if bboxes[2] > width:
+            print(width)
             print("resize xmax")
             bboxes[2] = width
         if bboxes[3] > height:
             print("resize ymax")
             bboxes[3] = height
-        annotations['bboxes'] = bboxes
+        new_annotations['bboxes'] = [bboxes]
+        if (len(annotations["keypoints"])) == 6:
+            print("new")
+            new_annotations["keypoints"] = [[annotations["keypoints"][0], annotations["keypoints"][1], annotations["keypoints"][2], annotations["keypoints"][3], annotations["keypoints"][4], annotations["keypoints"][5]]]
+        else:
+            new_annotations["keypoints"] = annotations["keypoints"]
+        # save new annotations
+        json_filename = path_to_annotations
+        with open(json_filename, 'w') as fp:
+            json.dump(new_annotations, fp)
+
 
 def main():
     # Path to the directory containing the annotations
